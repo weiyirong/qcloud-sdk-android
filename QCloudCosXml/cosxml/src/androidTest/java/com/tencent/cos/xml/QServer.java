fda201ac011232080ac488f00cb0134c1ae8bae8
bradyxiao <bradyxiao@tencent.com>
2019-04-22 22:07:49 +0800

    update ut

--- QCloudCosXml/cosxml/src/androidTest/java/com/tencent/cos/xml/QServer.java
+++ QCloudCosXml/cosxml/src/androidTest/java/com/tencent/cos/xml/QServer.java
@@ -1,7 +1,6 @@
 package com.tencent.cos.xml;
 
 import android.content.Context;
-import android.util.Log;
 
 import com.tencent.cos.xml.common.Region;
 import com.tencent.cos.xml.exception.CosXmlClientException;
@@ -9,16 +8,11 @@ import com.tencent.cos.xml.exception.CosXmlServiceException;
 import com.tencent.cos.xml.model.object.DeleteObjectRequest;
 import com.tencent.qcloud.core.auth.BasicQCloudCredentials;
 import com.tencent.qcloud.core.auth.ShortTimeCredentialProvider;
-import com.tencent.qcloud.core.http.QCloudHttpRetryHandler;
-import com.tencent.qcloud.core.task.RetryStrategy;
 
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.RandomAccessFile;
-
-import okhttp3.Request;
-import okhttp3.Response;
 
 /**
  * Created by bradyxiao on 2018/3/12.
@@ -28,40 +22,25 @@ public class QServer {
 
     public final static String TAG = "UnitTest";
 
+    public final static String ownUin = BuildConfig.OwnUin ;//"腾讯云账号";
     /** 腾讯云 cos 服务的 appid */
-    public final static String ownUin = BuildConfig.COSUin;
-    public final static String appid = BuildConfig.COSAppId;
-    public static final String secretId = BuildConfig.COSSecretId;
-    public static final String secretKey = BuildConfig.COSSecretKey;
-    /** persistBucket 所处在的地域 */
-    public final static String region = Region.AP_Guangzhou.getRegion() ;
-    public static final String persistBucket = "android-ut-persist-bucket";
-    public static final String tempBucket = "android-ut-temp-bucket";
-    public static CosXmlService cosXml;
+    public final static String appid = BuildConfig.APPID; //"腾讯云appid";
+    public static final String secretId = BuildConfig.SecretID; //"cos 服务密钥Id";
+    public static final String secretKey = BuildConfig.SecretKey; //"cos 服务密钥key";
+    public static final String bucketForBucketAPITest = BuildConfig.BucketForBucketTest; //"android-demo-ap-guangzhou";
+    /** bucketForObjectAPITest 所处在的地域 */
+    public final static String region = BuildConfig.Region; //Region.AP_Guangzhou.getRegion() ;
+    public static String bucketForObjectAPITest = BuildConfig.BucketForObjectTest; //"xmlandroidtest";
+    public static CosXml cosXml;
 
-    public static boolean cspTest = false;
     private QServer(Context context){
         CosXmlServiceConfig cosXmlServiceConfig = new CosXmlServiceConfig.Builder()
                 .isHttps(false)
                 .setAppidAndRegion(appid, region)
                 .setDebuggable(true)
-                .setRetryHandler(new QCloudHttpRetryHandler(){@Override public boolean shouldRetry(Request request, Response response, Exception exception){
-                    Log.e(TAG, request.url().host());
-                    try {
-                        cosXml.addCustomerDNS("service.cos.myqcloud.com", new String[]{"123.151.157.147"});
-                    } catch (CosXmlClientException e) {
-                        e.printStackTrace();
-                    }
-                    return true;
-                } })
                 .builder();
         cosXml = new CosXmlService(context, cosXmlServiceConfig,
                 new ShortTimeCredentialProvider(secretId,secretKey,600) );
-        try {
-            cosXml.addCustomerDNS("service.cos.myqcloud.com", new String[]{"123.159.157.147"});
-        } catch (CosXmlClientException e) {
-            e.printStackTrace();
-        }
     }
 
     public static void init(Context context){
